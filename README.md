f8ffcaf37213510d127fdef0d7b41b654ab52fb8
David Chodur <David.Chodur@lmc.eu>
2019-03-18 15:41:18 +0100

    AerospikeCache implementation

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Aerospike cache adapters for PHP
 [![Build Status](https://travis-ci.org/lmc/aerospike-cache-php.svg?branch=master)](https://travis-ci.org/lmc/aerospike-cache-php)
 [![Coverage Status](https://coveralls.io/repos/github/lmc/aerospike-cache-php/badge.svg?branch=master)](https://coveralls.io/github/lmc/aerospike-cache-php?branch=master)
 
-TODO description
+This component provides a PSR-6 implementation of Aerospike for adding cache to your applications. 
 
 ## Installation
 
@@ -14,8 +14,13 @@ composer require lmc/aerospike-cache
 ```
 
 ## Usage
+AerospikeCache takes as a parameters Aerospike Object, namespace with optional set, cache namespace and default TTL.
+```php
+$aerospike = new \Aerospike(['hosts'=>[['addr'=>'127.0.0.1', 'port'=>3000]]]);
+$aerospikeCache = new AerospikeCache($aerospike, 'aerospkeNamespace', 'aerospikeSet', 'cacheNamespace', 3600);
+```
 
-TODO usage
+AerospikeCache uses PSR-6 caching interface for manipulation with data.  
 
 ## Changelog
 For latest changes see [CHANGELOG.md](CHANGELOG.md) file. We follow [Semantic Versioning](https://semver.org/).
---
+++ src/AerospikeCache.php
@@ -0,0 +1,133 @@
+<?php
+declare(strict_types=1);
+
+namespace Lmc\AerospikeCache;
+
+use Aerospike;
+use Symfony\Component\Cache\Adapter\AbstractAdapter;
+use Symfony\Component\Cache\Exception\CacheException;
+
+class AerospikeCache extends AbstractAdapter
+{
+    private const WRAPPER_NAME = 'data';
+
+    /** @var Aerospike */
+    private $aerospike;
+
+    /** @var string $aerospikeNamespace */
+    private $namespace;
+
+    /** @var string $set */
+    private $set;
+
+    public function __construct(
+        \Aerospike $aerospike,
+        string $namespace,
+        string $set = 'cache',
+        string $cacheNamespace = '',
+        int $defaultLifetime = 0
+    ) {
+        $this->aerospike = $aerospike;
+        $this->namespace = $namespace;
+        $this->set = $set;
+        parent::__construct($cacheNamespace, $defaultLifetime);
+    }
+
+    protected function doFetch(array $ids): array
+    {
+        $result = [];
+
+        $keys = $this->initializeKeysForAerospike($ids);
+
+        $this->aerospike->getMany($keys, $records);
+
+        foreach ($records as $record) {
+            if ($record['metadata'] !== null) {
+                $result[$record['key']['key']] = isset($record['bins'][self::WRAPPER_NAME]) ?
+                    $record['bins'][self::WRAPPER_NAME] : null;
+            }
+        }
+
+        return $result;
+    }
+
+    private function initializeKeysForAerospike(array $ids): array
+    {
+        return array_reduce(
+            $ids,
+            function (array $values, $id) {
+                $values[] = $this->createKey($id);
+
+                return $values;
+            },
+            []
+        );
+    }
+
+    protected function doHave($id)
+    {
+        $statusCode = $this->aerospike->get($this->createKey($id), $read);
+
+        return $statusCode === \Aerospike::OK;
+    }
+
+    protected function doClear($namespace = ''): bool
+    {
+        if ($namespace === '') {
+            $statusCode = $this->aerospike->truncate($this->namespace, $this->set, 0);
+        } else {
+            $clearNamespace = function ($record) use ($namespace): void {
+                if ($namespace === mb_substr($record['key']['key'], 0, mb_strlen($namespace))) {
+                    $status = $this->aerospike->remove($record['key']);
+                    if (!$this->isStatusOkOrNotFound($status)) {
+                        throw new CacheException($this->aerospike->error());
+                    }
+                }
+            };
+
+            $this->aerospike->scan($this->namespace, $this->set, $clearNamespace);
+
+            $statusCode = 0;
+        }
+
+        return $this->isStatusOkOrNotFound($statusCode);
+    }
+
+    protected function doDelete(array $ids): bool
+    {
+        $removedAllItems = true;
+
+        foreach ($ids as $id) {
+            $statusCode = $this->aerospike->remove($this->createKey($id));
+            if (!$this->isStatusOkOrNotFound($statusCode)) {
+                $removedAllItems = false;
+            }
+        }
+
+        return $removedAllItems;
+    }
+
+    protected function doSave(array $values, $lifetime): bool
+    {
+        foreach ($values as $key => $value) {
+            $data = [self::WRAPPER_NAME => $value];
+            $statusCode = $this->aerospike->put($this->createKey($key), $data, $lifetime, [\Aerospike::OPT_POLICY_KEY => \Aerospike::POLICY_KEY_SEND]);
+        }
+
+        return $statusCode === \Aerospike::OK;
+    }
+
+    private function createKey($key)
+    {
+        return $this->aerospike->initKey($this->namespace, $this->set, $key);
+    }
+
+    private function isStatusOkOrNotFound(int $statusCode): bool
+    {
+        if ($statusCode === \Aerospike::ERR_RECORD_NOT_FOUND || $statusCode === \Aerospike::OK) {
+            return true;
+        }
+
+        return false;
+    }
+}
